9460f2c9ec7ae6172a002c23b97fd0bc6cc7d520
wadood <wadood2003@gmail.com>
2025-06-21 17:59:54 +0500

    Initial commit

---
+++ src/app/about/mission/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutMissionPage() {
+    return (
+        <div>
+            <h1>This is our mission at PrintForge...</h1>
+        </div>
+    )
+}
---
+++ src/app/models/page.tsx
@@ -0,0 +1,7 @@
+export default function ModelsPage() {
+    return (
+        <div>
+            <h1>Models</h1>
+        </div>
+    )
+}
